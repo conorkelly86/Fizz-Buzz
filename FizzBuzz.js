a7d3515303205a66dc4d839e9a4698053e40bab7
Conor Kelly <conorkelly86@gmail.com>
2018-01-22 11:13:53 +0000

    Comments Added

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,15 +1,15 @@
-for (i=1; i<=100; i++) {
-    if (i % 15 === 0){
+for (i=1; i<=100; i++) {                                        //start, stop, increment
+    if (i % 15 === 0){                                          //for numbers that are divisable by 3 & 5, output FizzBuzz, all numbers divisable by both 3 and 5 are also divisable by 15
         console.log("FizzBuzz");
     }
-    else if (i % 5 === 0) {
+    else if (i % 5 === 0) {                                     //for numbers divisable by 5, output Buzz
         console.log("Buzz");
     }
-    else if(i % 3 === 0)  {
+    else if(i % 3 === 0)  {                                     //for numbers divisable by 3, output Fizz
         console.log("Fizz");
     } 
     
-   else {
+   else {                                                       //if numbers not divisable by 3 or 5, output as normal
        console.log(i);
    }
 }
